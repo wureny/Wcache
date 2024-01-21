0e704898c2c4f6a5a5136d7fc735e558b4f95a2b
wureny <wurenyu19@gmail.com>
2024-01-21 09:21:28 +0800

    feat: cache.go-延迟初始化

--- cache.go
+++ cache.go
@@ -13,10 +13,11 @@ type Cache struct {
 	cacheBytes int64
 }
 
-func NewCache(maxBytes int64, m *sync.Mutex) *Cache {
+// l可以为nil，表示使用延迟初始化的方案
+func NewCache(l *lru.Cache, maxBytes int64) *Cache {
 	return &Cache{
-		lru:        lru.NewCache(maxBytes, nil),
-		mu:         m,
+		lru:        l,
+		mu:         &sync.Mutex{},
 		cacheBytes: maxBytes,
 	}
 }
@@ -24,12 +25,20 @@ func NewCache(maxBytes int64, m *sync.Mutex) *Cache {
 func (c *Cache) Add(key string, value ByteView) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	//延迟初始化
+	if c.lru == nil {
+		c.lru = lru.NewCache(c.cacheBytes, nil)
+	}
 	c.lru.Add(key, value)
 }
 
 func (c *Cache) Get(key string) (value ByteView, ok bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	//延迟初始化
+	if c.lru == nil {
+		return
+	}
 	if v, ok := c.lru.Get(key); ok {
 		return v.(ByteView), ok
 	}
